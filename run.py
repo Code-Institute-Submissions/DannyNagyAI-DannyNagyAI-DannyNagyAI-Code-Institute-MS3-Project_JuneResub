5963515e37200bdb4476ce916c4e5e6ea8586b0e
DannyNagyAI <danny.nagy@icloud.com>
2021-03-08 14:36:24 +0000

    created an *app.route* to about.html page.

--- run.py
+++ run.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
     
 
 if __name__ == "__main__":
