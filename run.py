737292e097686a1b7bc80e78bc4fc353c8e1e0bf
DannyNagyAI <danny.nagy@icloud.com>
2021-03-08 14:43:15 +0000

    added an *app.route* to recipes.html. A page which will store all the recipes.

--- run.py
+++ run.py
@@ -11,9 +11,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
     
 
 if __name__ == "__main__":
