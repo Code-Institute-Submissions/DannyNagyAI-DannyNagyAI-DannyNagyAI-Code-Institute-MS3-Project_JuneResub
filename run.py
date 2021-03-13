def9a387738d3a7ad60602f312fd4bdca30e5c47
DannyNagyAI <danny.nagy@icloud.com>
2021-03-13 14:42:09 +0000

    Added support for login and logout and made menu interactive when loged in or not

--- run.py
+++ run.py
@@ -30,23 +30,50 @@ def sandwiches():
     catogories_1 = categories_collection.find()
     return render_template("sandwiches.html", sandwiches=sandwiches_1, categories=catogories_1)
 
+
+# ------------------------------------------------------------------------------------------------------------
+# login
+# ------------------------------------------------------------------------------------------------------------
+
 @app.route("/login_form", methods=['GET']) 
 def login_form():
     return render_template("login_form.html")
 
 @app.route("/login", methods=['POST']) 
 def login():
+    # 1. Get the username and password provided from the loginform
     user_name = request.form["user_name"]
     password = request.form["password"]
 
+    # 2. Try to find a matching user from the given username/pwd
     user = users_collection.find_one({"user_name":user_name, "password":password})
     if(user is not None):
+        # User is now loged in, setting som session variables (might wanna replace later on)
         session['display_name'] = user.get("display_name")
-        return render_template("profile.html")
+        session['user_name'] = user.get("user_name")
+
+        # We also want to provide the profile page with the current loged in users sandwiches
+        user_sandwiches = sandwich_collection.find({"user_name":user.get("user_name")})
+
+        # Then we send the loged in user to his profile page with his sandwiches to go :-) 
+        return render_template("profile.html", user_sandwiches=user_sandwiches)
     else:
         message = "Could not log in user, please try again!"
         return render_template("login_form.html", message=message)
 
+
+@app.route('/logout')
+def logout():
+   # remove the username from the session if it is there
+   session.pop('user_name', None)
+   session.pop('display_name', None)
+   message = "You have been logged out"
+   return render_template("login_form.html", message=message)
+
+
+# ------------------------------------------------------------------------------------------------------------
+# Signup
+# ------------------------------------------------------------------------------------------------------------
 @app.route("/signup_form") 
 def signup_form():
     return render_template("signup_form.html")
@@ -68,28 +95,66 @@ def signup():
         message = "The registration went well, user " + display_name + " is now a member"
         return render_template("signup_form.html", message=message)
 
-
+# ------------------------------------------------------------------------------------------------------------
+# Profile Page
+# ------------------------------------------------------------------------------------------------------------
 @app.route("/profile")
 def profile():
+    # 1. Get the current loged in user 
+    user_name = session["user_name"]     # Note: add som error handeling!
+
+    # We also want to provide the profile page with the current loged in users sandwiches
+    user_sandwiches = sandwich_collection.find({"user_name": user_name})
+
+    # Then we send the loged in user to his profile page with his sandwiches to go :-) 
+    return render_template("profile.html", user_sandwiches=user_sandwiches)
+
+@app.route("/edit_sandwich")    
+def edit_sandwich():    
+    #Deleting a Task with various references    
+    #key=request.values.get("_id")    
+    #sandwich_collection.remove({"_id":ObjectId(key)})    
     return render_template("profile.html")
 
 
+@app.route("/delete_sandwich")    
+def delete_sandwich():    
+    #Deleting a Task with various references    
+    #key=request.values.get("_id")    
+    #sandwich_collection.remove({"_id":ObjectId(key)})    
+    return render_template("profile.html")
+
+
+# ------------------------------------------------------------------------------------------------------------
+# Sandwich Page
+# ------------------------------------------------------------------------------------------------------------
 @app.route("/sandwich_form")
 def sandwich_form():
     return render_template("sandwich_form.html")
 
+
 @app.route("/add_sandwich", methods=['POST'])
 def add_sandwich(): 
-    #Adding a sandwich  
+    # 1. Get the form data for the new sandwich  
     sandwich_name = request.form["sandwich_name"]
     description = request.form["description"]
     category = request.form["category"]
     ingredients = request.form["ingredients"]
     instructions = request.form["instructions"]
     image = request.form["image"]
-    sandwich_collection.insert({ "sandwich_name":sandwich_name, "description":description, "category":category, "ingredients":ingredients, "instructions":instructions, "image":image})    
-    return render_template("sandwich_form.html")
 
+    # 2. Save the new sandwich to database
+    sandwich_collection.insert({ "sandwich_name":sandwich_name, "description":description, "category":category, "ingredients":ingredients, "instructions":instructions, "image":image, "user_name":session['user_name']})    
+ 
+    # 3. Send the user back to profile page with a success-message and a list of the users sandwiches
+    message = "Sandwich successfully added"
+    user_sandwiches = sandwich_collection.find({"user_name": session["user_name"]})
+
+    return render_template("profile.html", message = message, user_sandwiches = user_sandwiches)
+
+# ------------------------------------------------------------------------------------------------------------
+# Main
+# ------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     app.run(
